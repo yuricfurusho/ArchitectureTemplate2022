f3ff0c58ce690b25ac0002db3faac855453ec097
Yuri Furusho <yuricfurusho@gmail.com>
2022-07-26 16:19:15 -0600

    AAT-2: Part 3: Adds Contractor List UI with Mock HardCoded Data

---
+++ ui-compose/src/main/java/com/yuricfurusho/architecturetemplate2022/ui/ContractorsScreen.kt
@@ -0,0 +1,47 @@
+package com.yuricfurusho.architecturetemplate2022.ui
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun ContractorsScreen() {
+    ContractorList(contractorList)
+}
+
+@Composable
+fun ContractorList(contractorList: List<ContractorItemUI>) {
+    LazyColumn {
+        items(contractorList) { contractor ->
+            ContractorItem(contractor)
+        }
+    }
+}
+
+@Composable
+fun ContractorItem(contractor: ContractorItemUI) {
+    Row(modifier = Modifier.background(contractor.categoryColour)) {
+        Text(
+            text = contractor.name,
+            modifier = Modifier
+                .padding(start = 16.dp)
+                .weight(1f)
+        )
+        Image(
+            contentDescription = contractor.cityName,
+            painter = painterResource(id = contractor.cityImage),
+            modifier = Modifier
+                .padding(end = 16.dp, start = 4.dp)
+                .size(100.dp)
+        )
+    }
+}
