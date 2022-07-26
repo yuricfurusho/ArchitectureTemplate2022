0123885ae6bbc17ea0ea43d766540f35d715fe0b
Yuri Furusho <yuricfurusho@gmail.com>
2022-07-26 16:19:15 -0600

    AAT-2: Part 3: Adds Contractor List UI with Mock HardCoded Data

---
+++ ui-compose/src/main/java/com/yuricfurusho/architecturetemplate2022/ui/ContractorItemUI.kt
@@ -0,0 +1,10 @@
+package com.yuricfurusho.architecturetemplate2022.ui
+
+import androidx.compose.ui.graphics.Color
+
+data class ContractorItemUI(
+    val name: String,
+    val categoryColour: Color,
+    val cityName: String,
+    val cityImage: Int
+)
