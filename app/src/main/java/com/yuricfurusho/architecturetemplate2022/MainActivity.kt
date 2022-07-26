18f1b6f5cfeb28cc5de77eb4439121617cc0b882
Yuri Furusho <yuricfurusho@gmail.com>
2022-07-26 13:59:10 -0600

    AAT-2: Part 1: Setting up backup XML UI

--- app/src/main/java/com/yuricfurusho/architecturetemplate2022/MainActivity.kt
+++ app/src/main/java/com/yuricfurusho/architecturetemplate2022/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.yuricfurusho.architecturetemplate2022
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.yuricfurusho.architecturetemplate2022.ui.ContractorsListActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        startActivity(Intent(this, ContractorsListActivity::class.java))
+        finish()
     }
 }
