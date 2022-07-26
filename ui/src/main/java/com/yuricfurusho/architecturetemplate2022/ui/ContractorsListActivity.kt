566fe5fc3344fddaa72baa60405a205d1b901a57
Yuri Furusho <yuricfurusho@gmail.com>
2022-07-26 13:59:10 -0600

    AAT-2: Part 1: Setting up backup XML UI

---
+++ ui/src/main/java/com/yuricfurusho/architecturetemplate2022/ui/ContractorsListActivity.kt
@@ -0,0 +1,11 @@
+package com.yuricfurusho.architecturetemplate2022.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ContractorsListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_contractors_list)
+    }
+}
